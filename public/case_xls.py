15585fd17bb79f7523dc4b8f898c49908e843b69
赵志权 <zhaozhiquan@zhaozhiquandeMac-mini.local>
2017-02-28 13:22:30 +0800

    2

--- public/case_xls.py
+++ public/case_xls.py
@@ -7,21 +7,21 @@ class Case_xls(object):
 	def get_row(self,values):
 		self.values = values
 		#获取第N行内容
-		self.xl = xlrd.open_workbook(os.getcwd() + os.sep + 'iOSSdk_case.xlsx')
+		self.xl = xlrd.open_workbook(os.getcwd() + os.sep + 'public/iOSSdk_case.xlsx')
 		self.table = self.xl.sheets()[0]
 		self.row = self.table.row_values(self.values)
 		return self.row
 	def get_col(self,values):
 		self.values = values
 		#获取第N列内容
-		self.xl = xlrd.open_workbook(os.getcwd()+os.sep+'iOSSdk_case.xlsx')
+		self.xl = xlrd.open_workbook(os.getcwd()+os.sep+'public/iOSSdk_case.xlsx')
 		self.table = self.xl.sheets()[0]
 		self.col = self.table.col_values(self.values)
 		return self.col
 	def get_danyuange(self,hang,lie):
 		self.hang = hang
 		self.lie = lie
-		self.xl = xlrd.open_workbook(os.getcwd() + os.sep + 'iOSSdk_case.xlsx')
+		self.xl = xlrd.open_workbook(os.getcwd() + os.sep + 'public/iOSSdk_case.xlsx')
 		self.table = self.xl.sheets()[0]
 		b = self.table.cell(self.hang,self.lie).value.encode('utf-8')
 		return b
